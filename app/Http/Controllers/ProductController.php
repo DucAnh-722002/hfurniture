e114c4bd9c769f3921ae731a7d317f7ca2c95219
DucAnh-722002 <93500771+DucAnh-722002@users.noreply.github.com>
2022-09-21 21:17:23 +0700

    Update ProductController.php

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -1,24 +1,15 @@
 <?php
 
 namespace App\Http\Controllers;
-use Illuminate\Support\Facades\DB;
+
 use Illuminate\Http\Request;
-use Illuminate\Support\Collection;
+use DB;
 
-class ProductController extends Controller
+class productController extends Controller
 {
-    public function Products(){
-        $popular1 = DB::table('products')->orderBy('viewers','asc')->where('status',1)->limit(7)->get();
-        $popular = $popular1->slice(0,3);
-        $popular2 = $popular1->slice(4,6);
-        $newest1 = DB::table('products')->orderBy('created_at','asc')->where('status',1)->limit(7)->get();
-        $newest = $newest1->slice(0,3);
-        $newest2 = $newest1->slice(4,6);
-        $sold1= DB::table('products')->where('status',1 and 'featured',1)->limit(7)->get();
-        $sold = $sold1->slice(0,3);
-        $sold2 = $sold1->slice(4,6);
-        $data = ['popular'=>$popular, 'newest'=>$newest,'sold'=>$sold,'popular2'=>$popular2,'newest2'=>$newest2,'sold2'=>$sold2];
-        return view('client/index',$data);
+    function details($id=0){
+        $products=DB::table('products')->where('id',$id)->first();
+        $data = ['products'=>$products];
+        return view('client/products/details',$data);
     }
-
 }
